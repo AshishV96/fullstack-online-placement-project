e7723f4fa960530b46520e5bd4636050a82f0016
Ashish <avchandan1@gmail.com>
2023-07-31 01:26:45 +0530

    jwt authentication

--- FrontEnd/src/token/tokenManager.js
+++ FrontEnd/src/token/tokenManager.js
@@ -1,3 +1,37 @@
 import jwt_decode from 'jwt-decode'
 
 export const tokenName = 'token'
+
+export function encryptToken(token) {
+    let encrypted = token.replaceAll('b', '!').replaceAll('i', '@').replaceAll('j', '#')
+        .replaceAll('m', '%').replaceAll('n', '^').replaceAll('s', '&').replaceAll('w', '*')
+
+    return encrypted
+}
+
+export function decryptToken(token) {
+    let decrypted = token.replaceAll('!', 'b').replaceAll('@', 'i').replaceAll('#', 'j')
+        .replaceAll('%', 'm').replaceAll('^', 'n').replaceAll('&', 's').replaceAll('*', 'w')
+
+    return decrypted
+}
+
+export function getBody() {
+    if ((token = localStorage.getItem(tokenName)) != null)
+        return jwt_decode('.' + decryptToken(token).split('.')[1] + '.')
+
+    else
+        return null
+}
+
+export function setToken(token) {
+    localStorage.setItem(tokenName, encryptToken(token))
+}
+
+export function getToken() {
+    if ((token = localStorage.getItem(tokenName)) != null)
+        return decryptToken(token)
+
+    else
+        return null
+}
